5204fc9490aa1510b7699b311a93672ea807bf93
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-01-30 22:53:20 -0800

    TestNoteGroup - change variable name

--- Sourcecode/LyreTest/Tests/TestNoteGroup.cpp
+++ Sourcecode/LyreTest/Tests/TestNoteGroup.cpp
@@ -46,116 +46,116 @@ namespace
 
 TEST( Constructor, NoteGroup )
 {
-    NoteGroup ngrp;
+    NoteGroup noteGroup;
     CHECK( true );
 }
 
 TEST( createNoteGroup, NoteGroup )
 {
-    INoteGroupUP ngrp = createNoteGroup();
-    CHECK( ngrp != nullptr );
+    INoteGroupUP noteGroup = createNoteGroup();
+    CHECK( noteGroup != nullptr );
 }
 
 
 TEST( clone, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.e2Sixteenth() );
-    INoteGroupUP cloned = ngrp->clone();
-    CHECK_EQUAL( 2, ngrp->getCount() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.e2Sixteenth() );
+    INoteGroupUP cloned = noteGroup->clone();
+    CHECK_EQUAL( 2, noteGroup->getCount() )
     CHECK_EQUAL( 2, cloned->getCount() )
-    CHECK( ngrp.get() != cloned.get() )
+    CHECK( noteGroup.get() != cloned.get() )
 }
 
 TEST( getIsEmpty_true, NoteGroup )
 {
-    INoteGroupUP ngrp = createNoteGroup();
-    CHECK( ngrp->getIsEmpty() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    CHECK( noteGroup->getIsEmpty() )
 }
 
 TEST( getIsEmpty_false, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
-    CHECK( ! ngrp->getIsEmpty() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
+    CHECK( ! noteGroup->getIsEmpty() )
 }
 
 TEST( getCount_0, NoteGroup )
 {
-    INoteGroupUP ngrp = createNoteGroup();
-    CHECK_EQUAL( 0, ngrp->getCount() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    CHECK_EQUAL( 0, noteGroup->getCount() )
 }
 
 TEST( getCount_1, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
-    CHECK_EQUAL( 1, ngrp->getCount() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
+    CHECK_EQUAL( 1, noteGroup->getCount() )
 }
 
 TEST( getCount_2, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
-    ngrp->add( f.e2Sixteenth() );
-    CHECK_EQUAL( 2, ngrp->getCount() )
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.e2Sixteenth() );
+    CHECK_EQUAL( 2, noteGroup->getCount() )
 }
 
 TEST( getDurationValue_0, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     Rational expected{ 0, 1 };
-    Rational actual = ngrp->getDurationValue();
+    Rational actual = noteGroup->getDurationValue();
     CHECK_EQUAL( expected, actual )
 }
 
 TEST( getDurationValue_Q, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
     Rational expected{ 1, 1 };
-    Rational actual = ngrp->getDurationValue();
+    Rational actual = noteGroup->getDurationValue();
     CHECK_EQUAL( expected, actual )
 }
 
 TEST( getDurationValue_QE, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.d4Eighth() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.d4Eighth() );
     Rational expected{ 3, 2 };
-    Rational actual = ngrp->getDurationValue();
+    Rational actual = noteGroup->getDurationValue();
     CHECK_EQUAL( expected, actual )
 }
 
 TEST( getDurationValue_QES, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.d4Eighth() );
-    ngrp->add( f.e2Sixteenth() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.e2Sixteenth() );
     Rational expected{ 7, 4 };
-    Rational actual = ngrp->getDurationValue();
+    Rational actual = noteGroup->getDurationValue();
     CHECK_EQUAL( expected, actual )
 }
 
 TEST( getCurrent_exception, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     bool isExceptionThrown = false;
     try
     {
-        auto n = ngrp->getCurrent();
+        auto n = noteGroup->getCurrent();
     }
     catch (...)
     {
@@ -167,10 +167,10 @@ TEST( getCurrent_exception, NoteGroup )
 TEST( getCurrent, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
-    INoteSPC actualNote = ngrp->getCurrent();
+    noteGroup->add( expectedNote );
+    INoteSPC actualNote = noteGroup->getCurrent();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -178,12 +178,12 @@ TEST( getCurrent, NoteGroup )
 TEST( getNext_exception, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.e2Sixteenth() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.e2Sixteenth() );
     bool isExceptionThrown = false;
     try
     {
-        auto n = ngrp->getNext();
+        auto n = noteGroup->getNext();
     }
     catch (...)
     {
@@ -195,11 +195,11 @@ TEST( getNext_exception, NoteGroup )
 TEST( getNext, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
-    INoteSPC actualNote = ngrp->getNext();
+    noteGroup->add( expectedNote );
+    INoteSPC actualNote = noteGroup->getNext();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -207,12 +207,12 @@ TEST( getNext, NoteGroup )
 TEST( getPrevious_exception, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.e2Sixteenth() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.e2Sixteenth() );
     bool isExceptionThrown = false;
     try
     {
-        auto n = ngrp->getPrevious();
+        auto n = noteGroup->getPrevious();
     }
     catch (...)
     {
@@ -224,12 +224,12 @@ TEST( getPrevious_exception, NoteGroup )
 TEST( getPrevious, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
-    ngrp->add( f.d4Eighth() );
-    ngrp->next();
-    INoteSPC actualNote = ngrp->getPrevious();
+    noteGroup->add( expectedNote );
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->next();
+    INoteSPC actualNote = noteGroup->getPrevious();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -237,13 +237,13 @@ TEST( getPrevious, NoteGroup )
 TEST( first, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
-    ngrp->add( f.d4Eighth() );
-    ngrp->next();
-    ngrp->first();
-    INoteSPC actualNote = ngrp->getCurrent();
+    noteGroup->add( expectedNote );
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->next();
+    noteGroup->first();
+    INoteSPC actualNote = noteGroup->getCurrent();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -251,13 +251,13 @@ TEST( first, NoteGroup )
 TEST( last, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.c4Quarter() );
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
-    ngrp->last();
-    INoteSPC actualNote = ngrp->getCurrent();
+    noteGroup->add( expectedNote );
+    noteGroup->last();
+    INoteSPC actualNote = noteGroup->getCurrent();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -265,13 +265,13 @@ TEST( last, NoteGroup )
 TEST( next, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.d4Eighth() );
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.c4Quarter() );
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );
+    noteGroup->add( expectedNote );
     INoteSPC actualNote = nullptr;
-    do { actualNote = ngrp->getCurrent(); } while ( ngrp->next() );
+    do { actualNote = noteGroup->getCurrent(); } while ( noteGroup->next() );
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -279,13 +279,13 @@ TEST( next, NoteGroup )
 TEST( previous, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
+    INoteGroupUP noteGroup = createNoteGroup();
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );ngrp->add( f.d4Eighth() );
-    ngrp->add( f.c4Quarter() );
+    noteGroup->add( expectedNote );noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.c4Quarter() );
     INoteSPC actualNote = nullptr;
-    ngrp->last();
-    do { actualNote = ngrp->getCurrent(); } while ( ngrp->previous() );
+    noteGroup->last();
+    do { actualNote = noteGroup->getCurrent(); } while ( noteGroup->previous() );
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -293,18 +293,18 @@ TEST( previous, NoteGroup )
 TEST( jump, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
     INoteSP expectedNote = f.e2Sixteenth();
-    ngrp->add( expectedNote );ngrp->add( f.d4Eighth() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->last();
-    ngrp->jump( 3 );
-    INoteSPC actualNote = ngrp->getCurrent();
+    noteGroup->add( expectedNote );noteGroup->add( f.d4Eighth() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->last();
+    noteGroup->jump( 3 );
+    INoteSPC actualNote = noteGroup->getCurrent();
     CHECK_EQUAL( expectedNote.get(), actualNote.get() )
     CHECK_EQUAL( expectedNote->toString(), actualNote->toString())
 }
@@ -312,18 +312,18 @@ TEST( jump, NoteGroup )
 TEST( jump_exception, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->last();
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->last();
     bool isExceptionThrown = false;
     try
     {
-        ngrp->jump( 7 );
+        noteGroup->jump( 7 );
     }
     catch (...)
     {
@@ -335,17 +335,17 @@ TEST( jump_exception, NoteGroup )
 TEST( jump_exception_negative, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->last();
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->last();
     bool isExceptionThrown = false;
     try
     {
-        ngrp->jump( -1 );
+        noteGroup->jump( -1 );
     }
     catch (...)
     {
@@ -357,18 +357,18 @@ TEST( jump_exception_negative, NoteGroup )
 TEST( remove_exception, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
     auto notAddedUP = f.c4Quarter();
     auto notAddedSP = toShared( notAddedUP );
     bool isExceptionThrown = false;
     try
     {
-        ngrp->remove( notAddedSP );
+        noteGroup->remove( notAddedSP );
     }
     catch (...)
     {
@@ -380,23 +380,23 @@ TEST( remove_exception, NoteGroup )
 TEST( remove, NoteGroup )
 {
     Factories f;
-    INoteGroupUP ngrp = createNoteGroup();
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
-    ngrp->add( f.c4Quarter() );
+    INoteGroupUP noteGroup = createNoteGroup();
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
+    noteGroup->add( f.c4Quarter() );
     auto noteToRemoveUP = f.c4Quarter();
     auto noteToRemoveSP = toShared( noteToRemoveUP );
-    ngrp->add( noteToRemoveSP );
-    CHECK_EQUAL( 6, ngrp->getCount() )
-    ngrp->remove( noteToRemoveSP );
-    CHECK_EQUAL( 5, ngrp->getCount() )
-    ngrp->first();
-    for (int i=0; i<ngrp->getCount(); ++i)
+    noteGroup->add( noteToRemoveSP );
+    CHECK_EQUAL( 6, noteGroup->getCount() )
+    noteGroup->remove( noteToRemoveSP );
+    CHECK_EQUAL( 5, noteGroup->getCount() )
+    noteGroup->first();
+    for (int i=0; i<noteGroup->getCount(); ++i)
     {
-        auto current = ngrp->getCurrent();
+        auto current = noteGroup->getCurrent();
         CHECK( current.get() != noteToRemoveSP.get() )
-        ngrp->next();
+        noteGroup->next();
     }
 }
