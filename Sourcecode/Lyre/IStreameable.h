bbc4519e9dd19c4fc5407c03164ddd466e86adbf
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-02-06 09:17:10 -0800

    IStreamable

--- Sourcecode/Lyre/IStreameable.h
+++ Sourcecode/Lyre/IStreameable.h
@@ -2,6 +2,7 @@
 #pragma once
 #include "Lyre/Lyre.h"
 #include <iostream>
+#include <sstream>
 
 namespace Lyre
 {
@@ -11,11 +12,12 @@ namespace Lyre
     {
     public:
         virtual ~IStreamable() {}
-        virtual std::ostream& toStream( std::ostream& os ) = 0;
+        virtual std::ostream& toStream( std::ostream& os ) const = 0;
         virtual String toString() const { std::stringstream ss; toStream( ss ); return ss.str(); }
     };
-    
-    PUBLIC std::ostream& operator<<( std::ostream& os, const IStreamable& object )
+
+    PUBLIC inline std::ostream& operator<<(
+        std::ostream& os, const IStreamable& object )
     {
         return object.toStream( os );
     }
