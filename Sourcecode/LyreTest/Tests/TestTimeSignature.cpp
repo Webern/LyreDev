2a57e13d460c5741df97651b8fda9adb9d76379f
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-02-17 18:24:02 -0800

    TestTimeSignature - not working

--- Sourcecode/LyreTest/Tests/TestTimeSignature.cpp
+++ Sourcecode/LyreTest/Tests/TestTimeSignature.cpp
@@ -3,6 +3,10 @@
 #include "Lyre/Lyre.h"
 #include "Lyre/ITimeSignatureFactory.h"
 #include "Lyre/ITimeSignature.h"
+#include "Lyre/IBeatPatternFactory.h"
+#include "Lyre/IBeatPattern.h"
+#include "Lyre/IDurationFactory.h"
+#include "Lyre/IDuration.h"
 #include <sstream>
 
 using namespace Lyre;
@@ -30,6 +34,18 @@ TEST( getTotalDuration, TimeSignature )
     ITimeSignatureUP ts = factory->create( 9, 8 );
     Rational expected{ 9, 2 };
     Rational actual = ts->getTotalDuration();
+    CHECK_EQUAL( expected, actual )
+}
+T_END
+
+TEST( getBeatPattern, TimeSignature )
+{
+    auto beatPatternFactory = createBeatPatternFactory();
+    auto durationFactory = createDurationFactory( DurationFactoryType::Standard );
+    IBeatPatternUP beatPattern = beatPatternFactory->create( 2, durationFactory->createDuration( "Quarter", 1 ) );
+    ITimeSignatureUP ts = factory->create( std::move( beatPattern ), 6, 4 );
+    String expected = "";
+    String actual = ".";// = ts->getBeatPattern()->toString();
     CHECK_EQUAL( expected, actual )
 }
 T_END
