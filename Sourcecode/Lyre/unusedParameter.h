577a55ad9489453aaf52b41184afef4706754913
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-02-19 21:59:09 -0800

    visual studio

--- Sourcecode/Lyre/unusedParameter.h
+++ Sourcecode/Lyre/unusedParameter.h
@@ -2,4 +2,8 @@
 
 #define PRIVATE_UNUSED_PARAMETER_STRINGIFY(argument) #argument
 
+#ifndef _MSC_VER
 #define UNUSED_PARAMETER(arg) _Pragma(PRIVATE_UNUSED_PARAMETER_STRINGIFY(unused(arg)))
+#else
+#define UNUSED_PARAMETER(arg) (##arg);
+#endif
