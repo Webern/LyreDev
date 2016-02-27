dfd889d126eb47605ac030663c8e7f4493aef1c5
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-02-27 00:51:27 -0800

    NoteGroup stub and vcxprojs

--- Sourcecode/Lyre/NoteGroup.cpp
+++ Sourcecode/Lyre/NoteGroup.cpp
@@ -4,6 +4,75 @@
 
 namespace Lyre
 {
+	INoteGroupUP NoteGroup::clone() const
+	{
+		return INoteGroupUP{};
+	}
+
+	std::ostream& NoteGroup::toStream( std::ostream& os ) const
+	{
+		return os;
+	}
+
+	bool NoteGroup::getIsEmpty() const
+	{
+		return false;
+	}
+
+	int NoteGroup::getCount() const
+	{
+		return -2;
+	}
+
+	Rational NoteGroup::getTotalDuration() const
+	{
+		return Rational{ -1, 1 };
+	}
+
+	INoteUP NoteGroup::getNote( int noteIndex ) const
+	{
+		return INoteUP{};
+	}
+
+	void NoteGroup::add( const INoteUP& note )
+	{
+
+	}
+
+	void NoteGroup::remove( int noteIndex )
+	{
+
+	}
+
+	int NoteGroup::getGroupCount() const
+	{
+		return -2;
+	}
+
+	bool NoteGroup::getIsInGroup( int noteIndex ) const
+	{
+		return false;
+	}
+
+	int NoteGroup::getGroupIndex( int noteIndex ) const
+	{
+		return -2;
+	}
+
+	INoteGroupUP NoteGroup::getGroup( int groupIndex ) const
+	{
+		return INoteGroupUP{};
+	}
+
+	void NoteGroup::addGroup( const INoteGroupUP& group )
+	{
+
+	}
+
+	void NoteGroup::removeGroup( int subGroupIndex )
+	{
+
+	}
 
 }
 
