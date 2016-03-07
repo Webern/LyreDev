47ade17d335014c4b858d419bfcfaea14915a940
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-03-06 19:13:35 -0800

    Visual Studio

--- Visual Studio/build.bat
+++ Visual Studio/build.bat
@@ -8,14 +8,14 @@ IF "%1"=="vstest" (
 
 if %test%=="vstest" (
 	ECHO VS Test Build
-	msbuild.exe Lyre.sln /p:Platform=x64;Configuration=Debug;TestPlatform=vstest /m
-	msbuild.exe LyreTest.vcxproj /p:Platform=x64;Configuration=Debug;TestPlatform=vstest /target:messages /m
-	vstest.console.exe .\Outputs\x64_debug\LyreTest.dll /Platform:x64 /Framework:framework45 /Enablecodecoverage /InIsolation
+	msbuild.exe Lyre.sln /p:Platform=x64;Configuration=Release;TestPlatform=vstest /m
+	msbuild.exe LyreTest.vcxproj /p:Platform=x64;Configuration=Release;TestPlatform=vstest /target:messages /m
+	vstest.console.exe .\Outputs\x64_Release\LyreTest.dll /Platform:x64 /Framework:framework45 /Enablecodecoverage /InIsolation
 	)
 
 if %test%=="cpul" (
 	ECHO CppUnitLite Build
-	msbuild.exe Lyre.sln /p:Platform=x64;Configuration=Debug;TestPlatform=cpul /m
-	msbuild.exe LyreTest.vcxproj /p:Platform=x64;Configuration=Debug;TestPlatform=cpul /target:messages /m
-	.\Outputs\x64_debug\LyreTest.exe
+	msbuild.exe Lyre.sln /p:Platform=x64;Configuration=Release;TestPlatform=cpul /m
+	msbuild.exe LyreTest.vcxproj /p:Platform=x64;Configuration=Release;TestPlatform=cpul /target:messages /m
+	.\Outputs\x64_Release\LyreTest.exe
 	)
