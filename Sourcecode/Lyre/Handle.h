80654d85b13121c9c292deafe0f065a03ab0b8fb
Matthew James Briggs <matthew.james.briggs@gmail.com>
2016-03-20 07:19:20 -0700

    handle

--- Sourcecode/Lyre/Handle.h
+++ Sourcecode/Lyre/Handle.h
@@ -10,7 +10,7 @@ namespace Lyre
     {
     public:
         Handle( T* ptr )
-        :myPtr( ptr )
+        : myPtr( ptr )
         {}
         
         T& operator*()
@@ -42,17 +42,71 @@ namespace Lyre
         {
             return myPtr;
         }
+
+		operator bool() const
+		{
+			return myPtr != nullptr;
+		}
         
     private:
         T* myPtr;
     };
-    
+
+
+	template<typename T>
+	class Handle<std::unique_ptr<T>>
+	{
+	public:
+		Handle( const std::unique_ptr<T>& ptr )
+			: myPtr( ptr )
+		{}
+
+		T& operator*()
+		{
+			return *myPtr;
+		}
+
+		const T& operator*() const
+		{
+			return *myPtr;
+		}
+
+		T* const operator->()
+		{
+			return myPtr.get();
+		}
+
+		const T* const operator->() const
+		{
+			return *myPtr.get();
+		}
+
+		T* const get()
+		{
+			return myPtr.get();
+		}
+
+		const T* const get() const
+		{
+			return myPtr.get();
+		}
+
+		operator bool() const
+		{
+			return myPtr != nullptr;
+		}
+
+	private:
+		const std::unique_ptr<T>& myPtr;
+	};
+
+
     template<typename T>
     class HandleConst
     {
     public:
         HandleConst( T* ptr )
-        :myPtr( ptr )
+        : myPtr( ptr )
         {}
         
         HandleConst( Handle<T*> nonConstHandle )
@@ -68,9 +122,47 @@ namespace Lyre
         {
             return myPtr;
         }
+
+		operator bool() const
+		{
+			return myPtr != nullptr;
+		}
         
     private:
         const T* myPtr;
     };
+
+
+	template<typename T>
+	class HandleConst<std::unique_ptr<T>>
+	{
+	public:
+		HandleConst( const std::unique_ptr<T>& ptr )
+			: myPtr( ptr )
+		{}
+
+		const T& operator*() const
+		{
+			return *myPtr;
+		}
+
+		const T* const operator->() const
+		{
+			return *myPtr.get();
+		}
+
+		const T* const get() const
+		{
+			return myPtr.get();
+		}
+
+		operator bool() const
+		{
+			return myPtr != nullptr;
+		}
+
+	private:
+		const std::unique_ptr<T>& myPtr;
+	};
     
 }
--- Sourcecode/LyreTest/Tests/TestHandle.cpp
+++ Sourcecode/LyreTest/Tests/TestHandle.cpp
@@ -40,3 +40,67 @@ TEST( mutablePtrObj, Handle )
     CHECK_EQUAL( 11, h->getPrivateValue() )
 }
 T_END
+
+TEST( operatorBool, Handle )
+{
+	std::unique_ptr<TestObject> uptr{ new TestObject{} };
+	Handle<TestObject> h{ uptr.get() };
+	CHECK( h );
+	uptr.reset( nullptr );
+	CHECK( !h );
+}
+T_END
+
+TEST( uptr, Handle )
+{
+	std::unique_ptr<TestObject> uptr{ new TestObject{} };
+	Handle<std::unique_ptr<TestObject>> h( uptr );
+	CHECK( h );
+	CHECK_EQUAL( 0, h->getPrivateValue() );
+	auto ptr = uptr.get();
+	uptr.release();
+	delete ptr;
+	CHECK( !h );
+}
+T_END
+
+TEST( mutablePtrInt, HandleConst )
+{
+	int* x = new int{ 1 };
+	HandleConst<int> h{ x };
+	CHECK_EQUAL( 1, *h )
+	delete x;
+}
+T_END
+
+TEST( mutablePtrObj, HandleConst )
+{
+	std::unique_ptr<TestObject> uptr{ new TestObject{} };
+	HandleConst<TestObject> h{ uptr.get() };
+	CHECK_EQUAL( 0, h->publicValue )
+	CHECK_EQUAL( 0, h->getPrivateValue() )
+}
+T_END
+
+TEST( operatorBool, HandleConst )
+{
+	std::unique_ptr<TestObject> uptr{ new TestObject{} };
+	HandleConst<TestObject> h{ uptr.get() };
+	CHECK( h );
+	auto ptr = uptr.get();
+	uptr.release();
+	delete ptr;
+	CHECK( !h )
+}
+T_END
+
+TEST( uptr, HandleConst )
+{
+	std::unique_ptr<TestObject> uptr{ new TestObject{} };
+	Handle<std::unique_ptr<TestObject>> h( uptr );
+	CHECK( h );
+	CHECK_EQUAL( 0, h->getPrivateValue() );
+	uptr.reset( nullptr );
+	CHECK( !uptr );
+}
+T_END
